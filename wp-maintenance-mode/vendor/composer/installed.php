a8681ca80cdc3834ec179233bb61b8f2941b8f76
Sean Murphy <sean@iamseanmurphy.com>
2024-09-21 21:47:20 +0000

    2.6.12

---
+++ wp-maintenance-mode/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/wp-maintenance-mode',
+        'pretty_version' => '2.6.12',
+        'version' => '2.6.12.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.30',
+            'version' => '3.3.30.0',
+            'reference' => '801a07604a297f02de3067948176f91b4c84bd8a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/wp-maintenance-mode' => array(
+            'pretty_version' => '2.6.12',
+            'version' => '2.6.12.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
